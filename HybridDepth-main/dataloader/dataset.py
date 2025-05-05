25b1c62484fd13f64d5ec8032390ece9f2aecc6d
hieuthehungry <phamhieu30091997@gmail.com>
2025-05-05 10:58:06 +0700

    ADD: training code for KITTI Eigen

--- HybridDepth-main/dataloader/dataset.py
+++ HybridDepth-main/dataloader/dataset.py
@@ -6,6 +6,7 @@ import torch
 from typing import Tuple
 from dataloader.DDFF12.loader import DDFF12Loader
 from .NYU.loader import NYULoader
+from .KITTI.loader import KITTIEIGENLoader
 from dataloader.ARKitScenes.loader import ARKitScenesLoader
 from dataloader.smartphone.loader import SmartphoneLoader
 
@@ -158,6 +159,77 @@ class NYUDataModule(pl.LightningDataModule):
             )
         if stage == "test":
             self.test_dataset = NYULoader(
+                self.nyu_data_root,
+                img_size=self.image_size,
+                remove_white_border=self.remove_white_border,
+                n_stack=self.n_stack,
+                stage="test"
+            )
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.test_dataset, batch_size=1, num_workers=self.num_workers
+        )
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=1, num_workers=self.num_workers)
+
+class KITTIEIGENDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        nyuv2_data_root: str = "",
+        img_size: Tuple = (480, 640),
+        remove_white_border: bool = True,
+        batch_size: int = 32,
+        num_workers: int = 16,
+        use_labels: bool = True,
+        num_cluster: int = 5,
+        n_stack: int = 10,
+    ):
+        """Initialize the data module."""
+        super().__init__()
+        # Set the seed for PyTorch
+        # torch.manual_seed(42)
+        self.nyu_data_root = nyuv2_data_root
+        self.image_size = img_size
+        self.remove_white_border = remove_white_border
+        self.n_stack = n_stack
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.use_labels = use_labels
+        self.num_cluster = num_cluster
+
+        # dataset_train, dataset_valid = random_split(dataset, [0.9, 0.1])
+
+        # self.train_dataset = dataset_train
+        # self.valid_dataset = dataset_valid
+        # self.test_dataset = dataset_valid
+
+    def setup(self, stage: str):
+        if stage == "fit":
+            self.train_dataset = KITTIEIGENLoader(
+                self.nyu_data_root,
+                img_size=self.image_size,
+                remove_white_border=self.remove_white_border,
+                n_stack=self.n_stack,
+                stage="train"
+            )
+            self.test_dataset = KITTIEIGENLoader(
+                self.nyu_data_root,
+                img_size=self.image_size,
+                remove_white_border=self.remove_white_border,
+                n_stack=self.n_stack,
+                stage="test"
+            )
+        if stage == "test":
+            self.test_dataset = KITTIEIGENLoader(
                 self.nyu_data_root,
                 img_size=self.image_size,
                 remove_white_border=self.remove_white_border,
