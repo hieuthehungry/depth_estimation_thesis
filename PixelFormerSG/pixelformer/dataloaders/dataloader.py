4d8e838e565302e8bdf2e2a57cd92877b9f6605d
hieuthehungry <phamhieu30091997@gmail.com>
2025-06-24 05:30:49 +0700

    CHORE

--- PixelFormerSG/pixelformer/dataloaders/dataloader.py
+++ PixelFormerSG/pixelformer/dataloaders/dataloader.py
@@ -231,13 +231,13 @@ class DataLoadPreprocess(Dataset):
                 sample = {'image': image,  "scene_graph": scene_graph, 'focal': focal}
         
         if self.transform:
-            print("Did you get to this")
+            # print("Did you get to this")
             sample = self.transform(sample)
             sample["scene_graph"] = scene_graph
             sample['path'] =  image_path
             if  self.mode == 'online_eval':
                 sample["has_valid_depth"] = has_valid_depth
-        print(sample.keys())
+        # print(sample.keys())
         return sample
     
     def rotate_image(self, image, angle, flag=Image.BILINEAR):
