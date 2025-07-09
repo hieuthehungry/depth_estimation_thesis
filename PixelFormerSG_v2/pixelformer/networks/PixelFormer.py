7fb2ee9f7d84f5ca0f3eed72017642812667668b
hieuthehungry <phamhieu30091997@gmail.com>
2025-07-10 01:16:20 +0700

    CHORE

--- PixelFormerSG_v2/pixelformer/networks/PixelFormer.py
+++ PixelFormerSG_v2/pixelformer/networks/PixelFormer.py
@@ -168,8 +168,10 @@ class SceneGraphEncoder(nn.Module):
 
         outputs = []
         for b in range(B):
+            print("=========================")
             print(node_feat_list[b].shape)
             print(edge_attr_list[b].shape)
+            print("=========================")
             out = self.gat(node_feat_list[b], edge_index_list[b], edge_attr_list[b])
             outputs.append(out)
         return torch.stack(outputs, dim=0)  # [B, N_obj, D]
