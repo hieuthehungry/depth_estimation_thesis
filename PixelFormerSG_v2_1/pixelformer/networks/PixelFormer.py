66e4bbdfd6e35557eab024abdfbf859e7d170823
hieuthehungry <phamhieu30091997@gmail.com>
2025-07-16 00:07:55 +0700

    CHORE

--- PixelFormerSG_v2_1/pixelformer/networks/PixelFormer.py
+++ PixelFormerSG_v2_1/pixelformer/networks/PixelFormer.py
@@ -436,7 +436,7 @@ class PixelFormerSG(nn.Module):
         
         # scene graph encoder
         self.sg_encoder_q4 = SceneGraphEncoder(feat_dim=in_channels[3], node_dim=in_channels[3], out_dim=v_dims[3])
-        self.sg_builder = SceneGraphBuilder(iou_thresh=0.6)
+        self.sg_builder = SceneGraphBuilder(num_rel_classes = 51, iou_thresh=0.6)
         # self.sg_encoder_q3 = SceneGraphEncoder(node_dim=in_channels[2], out_dim=v_dims[2])
         # self.sg_encoder_q2 = SceneGraphEncoder(node_dim=in_channels[1], out_dim=v_dims[1])
         # self.sg_encoder_q1 = SceneGraphEncoder(node_dim=in_channels[0], out_dim=v_dims[0])
