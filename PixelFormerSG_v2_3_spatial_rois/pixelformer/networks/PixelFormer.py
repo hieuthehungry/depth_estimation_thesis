0a6462d85ca4a99347da391599a1ba712f7b3bc7
hieuthehungry <phamhieu30091997@gmail.com>
2025-07-21 14:55:57 +0700

    sg_spatial_roi

--- PixelFormerSG_v2_3_spatial_rois/pixelformer/networks/PixelFormer.py
+++ PixelFormerSG_v2_3_spatial_rois/pixelformer/networks/PixelFormer.py
@@ -310,9 +310,11 @@ class SceneGraphEncoder(nn.Module):
 
             edge_index = sg_data_list[b]["edge_index"].to(node_feats.device)
             enhanced_nodes = self.gnn(node_feats, edge_index, edge_attr)
+            print(enhanced_nodes.shape)
 
             for i in range(num_boxes):
                 x1, y1, x2, y2 = rois[i].long()
+                print(x1, y1, x2, y2)
                 h, w = y2 - y1, x2 - x1
                 roi_patch = enhanced_nodes[i].view(1, -1, 1, 1)
                 upsampled = F.interpolate(roi_patch, size=(h, w), mode='bilinear', align_corners=False)
